036fac932c7a6c1b8bb275a46a61bc91cf2fcb1d
ajmenG <115182664+ajmenG@users.noreply.github.com>
2025-05-05 01:17:24 +0200

    Revert "Cut edges optymalization"

--- include/graph.h
+++ include/graph.h
@@ -3,11 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-
-// Forward declaration - zastępuje #include "fm_optimization.h"
-struct Partition_data;
-typedef struct Partition_data Partition_data;
+#include "partition.h"
 
 typedef struct Node
 {
@@ -21,19 +17,10 @@ typedef struct Node
 typedef struct Graph
 {
     int vertices;
-    int edges;
-    int parts;
-    int min_count;
-    int max_count;
     Node *nodes;
 } Graph;
 
-// Prototypy funkcji
+int **get_part_neighbors(const Graph *graph, const Partition_data *partition_data, int part_id, int *size);
 void print_part_neighbors(int **neighbors, int size);
-void inicialize_graph(Graph *graph, int vertices);
-void count_edges(Graph *graph);
-void assign_min_max_count(Graph *graph, int parts, float accuracy);
-void assing_parts(Graph *graph, int parts);
-void free_graph(Graph *graph);
 
 #endif
